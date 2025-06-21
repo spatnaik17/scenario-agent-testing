2fade0b6eb1935b34c7c91bd75d57f6d058e298b
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2025-06-21 11:35:15 +0200

    docs: move some parts of writing scenarios actually to concepts, and adapt RefLink to take both python and typescript

--- docs/docs/components/RefLink.tsx
+++ docs/docs/components/RefLink.tsx
@@ -1,17 +1,17 @@
 export const RefLink = ({
   link,
-  children,
+  code,
 }: {
-  link: string;
-  children: React.ReactNode;
+  link: { python: string; typescript?: string };
+  code: { python: string; typescript?: string };
 }) => {
   return (
     <a
-      href={`/reference/python/scenario/${link}`}
+      href={`/reference/python/scenario/${link.python}`}
       target="reference"
       className="underline decoration-dotted decoration-gray-400 hover:no-underline"
     >
-      {children}
+      <code className="vocs_Code">{code.python}</code>
     </a>
   );
 };
