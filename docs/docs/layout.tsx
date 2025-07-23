25f23b65e04cd2893e122b782a3736e7aaeffe4c
Richard Huth <richhuth@gmail.com>
2025-07-23 12:50:33 +0200

    feat: add gtag (#112)
    add gtag

--- docs/docs/layout.tsx
+++ docs/docs/layout.tsx
@@ -1,8 +1,10 @@
 import { GithubStarsScript } from "./components/GithubStars";
+import { GoogleTagScript } from "./components/GoogleTagScript";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <GoogleTagScript trackingId="GTM-KJ4S6Z9C" />
       {children}
       <GithubStarsScript repo="langwatch/scenario" />
     </>
